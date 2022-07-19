c8e33994764b8dde6c62e4beb9a6867ca2a13bff
Abernita <rusdaminov@gmail.com>
2022-07-19 14:53:42 +0300

    day04

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -2,3 +2,39 @@
 // Created by Arshavin Bernita on 7/19/22.
 //
 
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	const WrongAnimal* k = new WrongCat();
+
+	std::cout << "Type Dog: " << j->getType() << " " << std::endl;
+	std::cout << "Type Cat: " << i->getType() << " " << std::endl;
+	std::cout << "Type WrongCat: " << k->getType() << " " << std::endl << std::endl;
+
+	std::cout << "myDog: ";
+	j->makeSound();
+
+	std::cout << "myCat: ";
+	i->makeSound();
+
+	std::cout << "myWrongCat: ";
+	k->makeSound();
+
+	std::cout << "myMetaClass: ";
+	meta->makeSound();
+
+	std::cout << std::endl;
+	delete j;
+	delete i;
+	delete k;
+	delete meta;
+
+	return (0);
+}
